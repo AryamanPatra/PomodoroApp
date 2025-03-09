459bf540b16e5c1bc0e100d7f3cf9e0676fa0485
Aryaman Patra <aryamanprivate26@gmail.com>
2025-03-10 02:26:19 +0530

    Doing before working on logic

--- app/src/main/java/com/aryaman/pomodoroapp/ui/screens/TimerScreen.kt
+++ app/src/main/java/com/aryaman/pomodoroapp/ui/screens/TimerScreen.kt
@@ -1,13 +1,69 @@
 package com.aryaman.pomodoroapp.ui.screens
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.PlayArrow
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.IconButtonColors
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 
 @Composable
 fun TimerScreen(navController: NavHostController, innerPadding: PaddingValues) {
-    Text("Timer", modifier = Modifier.padding(innerPadding))
+    Column(
+        modifier = Modifier.padding(innerPadding).fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            modifier = Modifier,
+            text = "Sessions",
+            style = MaterialTheme.typography.titleMedium
+        )
+        Text(
+            modifier = Modifier.padding(bottom = 25.dp),
+            text = "0",
+            style = MaterialTheme.typography.displayMedium,
+            fontWeight = FontWeight.SemiBold
+        )
+        IconButton(
+            modifier = Modifier
+                .size(130.dp),
+            onClick = {
+                //Todo
+            },
+            colors = IconButtonColors(
+                containerColor = MaterialTheme.colorScheme.primary,
+                contentColor = MaterialTheme.colorScheme.onPrimary,
+                disabledContainerColor = MaterialTheme.colorScheme.surfaceDim,
+                disabledContentColor = MaterialTheme.colorScheme.onSurface,
+            )
+        ) {
+            Icon(modifier = Modifier.size(100.dp),imageVector = Icons.Default.PlayArrow, contentDescription = "Play")
+        }
+        Text(
+            modifier = Modifier.padding(top = 25.dp),
+            text = "Focus Time",
+            style = MaterialTheme.typography.titleLarge
+        )
+        Text(
+            modifier = Modifier,
+            text = "25:00",
+            style = MaterialTheme.typography.displayLarge,
+            fontWeight = FontWeight.SemiBold
+        )
+
+    }
 }
