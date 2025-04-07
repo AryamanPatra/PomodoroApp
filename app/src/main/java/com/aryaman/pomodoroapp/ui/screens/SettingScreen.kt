5d9f44617d4a587f9c7493733e394af867835491
Aryaman Patra <aryamanprivate26@gmail.com>
2025-04-08 00:57:22 +0530

    WIP: Added UI code for ThemeDialog, still working on logic to change theme

--- app/src/main/java/com/aryaman/pomodoroapp/ui/screens/SettingScreen.kt
+++ app/src/main/java/com/aryaman/pomodoroapp/ui/screens/SettingScreen.kt
@@ -1,23 +1,49 @@
 package com.aryaman.pomodoroapp.ui.screens
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun SettingScreen(navController: NavHostController = rememberNavController()){
+fun SettingScreen(navController: NavHostController = rememberNavController()) {
     Scaffold(
         modifier = Modifier.fillMaxSize(),
         topBar = {
@@ -28,7 +54,10 @@ fun SettingScreen(navController: NavHostController = rememberNavController()){
                 },
                 navigationIcon = {
                     IconButton(onClick = { navController.popBackStack() }) {
-                        Icon(imageVector = Icons.AutoMirrored.Filled.ArrowBack, contentDescription = "Back")
+                        Icon(
+                            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                            contentDescription = "Back"
+                        )
                     }
                 },
                 actions = {},
@@ -39,6 +68,104 @@ fun SettingScreen(navController: NavHostController = rememberNavController()){
             )
         }
     ) {
+        val themeDialogState = remember { mutableStateOf(false) }
+        if (themeDialogState.value) {
+            ThemeDialog{
+                themeDialogState.value = false
+            }
+        }
+        Column(
+            modifier = Modifier
+                .padding(it)
+                .fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Top
+        ) {
+            Surface(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .wrapContentHeight()
+                    .clickable {
+                        themeDialogState.value = true
+                    }
+            ) {
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .wrapContentHeight()
+                        .padding(vertical = 15.dp, horizontal = 50.dp),
+                    horizontalAlignment = Alignment.Start,
+                    verticalArrangement = Arrangement.SpaceEvenly
+                ) {
+                    Text(
+                        text = "Theme",
+                        style = MaterialTheme.typography.titleMedium,
+                        fontWeight = FontWeight.W600
+                    )
+                    Spacer(modifier = Modifier.height(4.dp))
+                    Text(
+                        text = "Current theme",
+                        style = MaterialTheme.typography.titleSmall,
+                        color = MaterialTheme.colorScheme.onSurfaceVariant,
+                        fontWeight = FontWeight.W400
+                    )
+                }
+            }
+        }
+    }
+}
 
+@Preview
+@Composable
+private fun ThemeDialog(onDismissRequest: () -> Unit = {}) {
+    Dialog(
+        onDismissRequest = onDismissRequest,
+        properties = DialogProperties(
+            dismissOnBackPress = true,
+            dismissOnClickOutside = true,
+            usePlatformDefaultWidth = true
+        )
+    ) {
+        Surface(
+            modifier = Modifier
+                .padding(12.dp)
+                .wrapContentSize(),
+            shape = RoundedCornerShape(12),
+            color = MaterialTheme.colorScheme.surfaceContainer,
+        ) {
+            val themeChoices = listOf("Light","Dark","System Default")
+            Column(
+                modifier = Modifier
+                    .wrapContentSize()
+                    .padding(vertical = 15.dp, horizontal = 20.dp),
+                horizontalAlignment = Alignment.Start,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text(text = "Choose theme", style = MaterialTheme.typography.headlineMedium, fontWeight = FontWeight.W500)
+                Spacer(modifier = Modifier.height(10.dp))
+                LazyColumn(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .wrapContentHeight(),
+                    horizontalAlignment = Alignment.Start,
+                ) {
+                    items(themeChoices.size){ i ->
+                        Row(
+                            verticalAlignment = Alignment.CenterVertically,
+                            horizontalArrangement = Arrangement.Start
+                        ) {
+                            RadioButton(
+                                modifier = Modifier,
+                                selected = false,
+                                onClick = {
+
+                                },
+                            )
+                            Text(text = themeChoices[i])
+                        }
+                    }
+                }
+            }
+        }
     }
 }
